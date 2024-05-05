01145be14707301c1207b63af1a6e60dc6553a4a
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-05 15:06:57 +0530

    auto push

--- env-dev/dev.tfvars
+++ env-dev/dev.tfvars
@@ -1,4 +1,4 @@
 ENV                 = "dev"
-ZONE                = ["us-east-1a", "us-east-1b"]
+# ZONE                = ["us-east-1a", "us-east-1b"]
 
 
--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "tf-module-mongodb" {
   source              = "./vendor/modules/mongodb"
   ENV                 = var.ENV
-  ZONE                = var.ZONE
+  # ZONE                = var.ZONE
 }
 
 # module "tf-module-redis" {
