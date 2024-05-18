efc01889a01cad1c33e3c7f20ac09f055a567e8b
Saurabh Dighe <sd.saurabhdighe@gmail.com>
2024-05-19 03:27:02 +0530

    Auto push

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "DOCDB_ENDPOINT" {
   value = module.tf-module-mongodb.DOCDB_ENDPOINT
 }
+
+output "REDIS_ENDPOINT" {
+  value = module.tf-module-redis.DOCDB_ENDPOINT
+}
+
+output "MYSQL_ENDPOINT" {
+  value = module.tf-module-mysql.DOCDB_ENDPOINT
+}
